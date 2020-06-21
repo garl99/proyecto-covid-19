80c8a1e65881381a6ec0df86751422fb99f7f5e9
Gabriel Romero <garomerol.17@est.ucab.edu.ve>
2020-06-20 22:20:11 -0400

    ENT1:Merge con Daniel

---
+++ creates-inserts/secuencias.sql
@@ -0,0 +1,83 @@
+create sequence sec_Lugar
+start with 6
+increment by 1;
+
+create sequence sec_Patologia
+start with 1
+increment by 1;
+
+create sequence sec_persona
+start with 1
+increment by 1;
+
+create sequence sec_patologia_persona
+start with 1
+increment by 1;
+
+create sequence sec_Sintoma
+start with 1
+increment by 1;
+
+create sequence sec_sintoma_persona
+start with 1
+increment by 1;
+
+create sequence sec_centro_atencion
+start with 1
+increment by 1;
+
+create sequence sec_Insumo
+start with 1
+increment by 1;
+
+create sequence sec_Centro_Insumo
+start with 1
+increment by 1;
+
+create sequence sec_Ayuda_Humanitaria
+start with 1
+increment by 1;
+
+create sequence sec_Ayuda_Insumo
+start with 1
+increment by 1;
+
+create sequence sec_Aerolinea
+start with 1
+increment by 1;
+
+create sequence sec_Avion
+start with 1
+increment by 1;
+
+create sequence sec_viaje
+start with 1
+increment by 1;
+
+create sequence sec_Estatus
+start with 1
+increment by 1;
+
+create sequence sec_estatus_persona
+start with 1
+increment by 1;
+
+create sequence sec_Frontera
+start with 1
+increment by 1;
+
+create sequence sec_Modelo
+start with 1
+increment by 1;
+
+create sequence sec_Proveedor_Internet
+start with 1
+increment by 1;
+
+create sequence sec_Lugar_Proveedor
+start with 1
+increment by 1;
+
+create sequence sec_Modelo_Lugar
+start with 1
+increment by 1;
